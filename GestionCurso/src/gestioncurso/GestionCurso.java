1c073e02a282dd195983db159148cd0b6add22ab
alumno <alumno@aula01-14>
2016-12-19 19:51:11 +0100

    1

---
+++ GestionCurso/src/gestioncurso/GestionCurso.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestioncurso;
+
+
+
+/**
+ *
+ * @author alumno
+ */
+public class GestionCurso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Modulo[] vModulos=new Modulo[5];
+    
+    vModulos[0]=new Modulo();
+    vModulos[1]=new Modulo("programacion",200,"juan fran");
+    }
+    //Modulo modulo=new Modulo();
+    
+   
+}
